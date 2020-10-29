06d582edd13816a14e41976c476e715541bf035e
jroselli113 <jroselli113@gmail.com>
2020-10-29 00:02:04 -0700

    Added more to policies.js
    Worked on a possible way to do our porject.

--- policies.js
+++ policies.js
@@ -1,16 +1,36 @@
+var groceryArray =  [
+					["Safeway ", "https://www.safeway.com/faq/covid-19-faq.html"],
+					["Fry's ", "https://www.frysfood.com/i/coronavirus-update"],
+					["Target ", "https://corporate.target.com/about/purpose-history/our-commitments/target-coronavirus-hub"],
 
+					];
+
+var restArray = [
+				["Chick-fil-a", "https://www.chick-fil-a.com/covid"],
+				["P.F. Chang's", "https://www.pfchangs.com/updates"],
+				["Five Guys", "https://www.fiveguys.com/covid-19-statement"],
+				];
+
+var allArray = groceryArray.concat(restArray);
 
 
 
 function search() {  //Doesn't work yet, but will eventually print out covid policies or links to covid polices based on what the user selects
+	//window.alert("hi");
 	var x = document.getElementsByName('selection');
 	var y = "all";
-	for(i=0; i < x.length; i++){
-		document.write("1"); 
+	for(i=0; i < x.length; i++){ 
 		if(x[i].checked)
 			y = x[i].value;
 	}
 
-	document.write(y);
+	if (y == "all")
+		document.getElementById("out").innerHTML = allArray;
+	else if(y == "grocery")
+		document.getElementById("out").innerHTML = groceryArray;
+	else if(y == "restaurant")
+		document.getElementById("out").innerHTML = restArray;
+
+	//document.write(y);
 }
 
