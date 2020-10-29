ae62494a270ec6390e7a2b826fede3e3696f755a
jroselli113 <jroselli113@gmail.com>
2020-10-29 03:15:48 -0700

    Applied style.css and edited policies.js
    I applied style.css to about.html and CovidPolicies.html to make the website uniform. I also made the js output in CovidPolices neater.

--- policies.js
+++ policies.js
@@ -1,22 +1,23 @@
 var groceryArray =  [
-					["Safeway ", "https://www.safeway.com/faq/covid-19-faq.html"], 
-					["Fry's ", "https://www.frysfood.com/i/coronavirus-update"],
-					["Target ", "https://corporate.target.com/about/purpose-history/our-commitments/target-coronavirus-hub"],
+					["Safeway:", "https://www.safeway.com/faq/covid-19-faq.html"], 
+					["Fry's:", "https://www.frysfood.com/i/coronavirus-update"],
+					["Target:", "https://corporate.target.com/about/purpose-history/our-commitments/target-coronavirus-hub"],
 
 					];
 
 var restArray = [
-				["Chick-fil-a", "https://www.chick-fil-a.com/covid"],
-				["P.F. Chang's", "https://www.pfchangs.com/updates"],				// Do you think doing our project in this way will work?
-				["Five Guys", "https://www.fiveguys.com/covid-19-statement"],  		// I'm not sure the best way to go about implementing our idea, so feel free to alter this as much as you want
+				["Chick-fil-a:", "https://www.chick-fil-a.com/covid"],
+				["P.F. Chang's:", "https://www.pfchangs.com/updates"],				// Do you think doing our project in this way will work?
+				["Five Guys:", "https://www.fiveguys.com/covid-19-statement"],  		// I'm not sure the best way to go about implementing our idea, so feel free to alter this as much as you want
 				];
 
 var allArray = groceryArray.concat(restArray);
 
 
 
+
+
 function search() {  //Doesn't work yet, but will eventually print out covid policies or links to covid polices based on what the user selects
-	//window.alert("hi");
 	var x = document.getElementsByName('selection');
 	var y = "all";
 	for(i=0; i < x.length; i++){ 
@@ -24,13 +25,23 @@ function search() {  //Doesn't work yet, but will eventually print out covid pol
 			y = x[i].value;
 	}
 
-	if (y == "all")
-		document.getElementById("out").innerHTML = allArray;
-	else if(y == "grocery")
-		document.getElementById("out").innerHTML = groceryArray;
-	else if(y == "restaurant")
-		document.getElementById("out").innerHTML = restArray;
-
-	//document.write(y);
+	if (y == "all") {
+		var tempAll = "";
+		for(i = 0; i < allArray.length; i++)
+			tempAll += allArray[i][0] + "\t" + allArray[i][1] + '<br>';
+		document.getElementById("out").innerHTML = tempAll;
+	}
+	else if(y == "grocery") {
+		var tempGrocery = "";
+		for(i = 0; i < groceryArray.length; i++)
+			tempGrocery += groceryArray[i][0] + "\t" + groceryArray[i][1] + '<br>';
+		document.getElementById("out").innerHTML = tempGrocery;
+	}
+	else if(y == "restaurant") {
+		var tempRest = "";
+		for(i = 0; i < restArray.length; i++)
+			tempRest += restArray[i][0] + "\t" + restArray[i][1] + '<br>';
+		document.getElementById("out").innerHTML = tempRest;
+	}
 }
 
